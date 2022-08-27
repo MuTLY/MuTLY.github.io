63dde9a6d00ae119db9f613823b63795d13b2652
Leandro Barbosa <leandro.barbosa@live.com>
2022-08-27 20:04:26 -0300

    Commenting out image block; Prettier;

--- default.js
+++ default.js
@@ -1,137 +1,155 @@
-(function() {
-    'use strict';
+(function () {
+  "use strict";
 
-    (function externalLinks() {
-        if (document.querySelector) {
-            var i = document.querySelectorAll('a[rel=external]');
-            [].slice.call(i).forEach(function(i) {
-                i.target = '_blank';
-            });
-        }
-    }());
-
-    var isMobile = {
-        Windows: function() {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        Android: function() {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function() {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function() {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        iPad: function() {
-            return navigator.userAgent.match(/iPad/i);
-        },
-        iPhone: function() {
-            return navigator.userAgent.match(/iPhone/i);
-        },
-        Opera: function() {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Kindle: function() {
-            return navigator.userAgent.match(/Silk/i);
-        },
-        any: function() {
-            return isMobile.Windows() ||
-                   isMobile.Android() ||
-                   isMobile.BlackBerry() ||
-                   isMobile.iOS() ||
-                   isMobile.Opera() ||
-                   isMobile.Kindle();
-        }
-    };
-
-    var vWidth = window.innerWidth,
-        str = 'Leandro Rabello Barbosa',
-        d = document.querySelector('body'),
-        isFF = false;
-
-    if (!isMobile.any()) {
-        str = 'Ctrl + Shift + J';
+  (function externalLinks() {
+    if (document.querySelector) {
+      var i = document.querySelectorAll("a[rel=external]");
+      [].slice.call(i).forEach(function (i) {
+        i.target = "_blank";
+      });
     }
+  })();
 
-    if (navigator.userAgent.indexOf('Firefox') !== -1) {
-        str = "Ctrl + Shift + K";
-        isFF = true;
-    }
-    
-    if (navigator.userAgent.indexOf('Mac OS X') !== -1 && !isMobile.iOS()) {
-        //str = "⌘ + ⌥ + J";
-        str = 'Command + Option + J';
-    }
-    
-    if (navigator.userAgent.indexOf('Mac OS X') !== -1 &&
-        navigator.userAgent.indexOf('Safari') !== -1 &&
-        navigator.userAgent.indexOf('Chrome') === -1 &&
-        !isMobile.iOS()) {
-        str = "Command + Option + I";
-    }
+  var isMobile = {
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    iPad: function () {
+      return navigator.userAgent.match(/iPad/i);
+    },
+    iPhone: function () {
+      return navigator.userAgent.match(/iPhone/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Kindle: function () {
+      return navigator.userAgent.match(/Silk/i);
+    },
+    any: function () {
+      return (
+        isMobile.Windows() ||
+        isMobile.Android() ||
+        isMobile.BlackBerry() ||
+        isMobile.iOS() ||
+        isMobile.Opera() ||
+        isMobile.Kindle()
+      );
+    },
+  };
 
-    if ((vWidth <= 1024 && isMobile.any()) || isMobile.Kindle()) {
-        d.className = d.className + ' tablet';
-    }
+  var vWidth = window.innerWidth,
+    str = "Leandro Rabello Barbosa",
+    d = document.querySelector("body"),
+    isFF = false;
 
-    document.title = str;
+  if (!isMobile.any()) {
+    str = "Ctrl + Shift + J";
+  }
 
-    str = str.replace(/ \+/g, ' </button> + <button>');
-    str = '<button> ' + str + ' </button>';
+  if (navigator.userAgent.indexOf("Firefox") !== -1) {
+    str = "Ctrl + Shift + K";
+    isFF = true;
+  }
 
-    document.querySelector('.command').innerHTML = str;
+  if (navigator.userAgent.indexOf("Mac OS X") !== -1 && !isMobile.iOS()) {
+    //str = "⌘ + ⌥ + J";
+    str = "Command + Option + J";
+  }
 
-    // magic starts here
-    var weekday = function() {
-        var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-            now = new Date();
-        return dayNames[now.getDay()];
-    };
+  if (
+    navigator.userAgent.indexOf("Mac OS X") !== -1 &&
+    navigator.userAgent.indexOf("Safari") !== -1 &&
+    navigator.userAgent.indexOf("Chrome") === -1 &&
+    !isMobile.iOS()
+  ) {
+    str = "Command + Option + I";
+  }
 
-    var css;
+  if ((vWidth <= 1024 && isMobile.any()) || isMobile.Kindle()) {
+    d.className = d.className + " tablet";
+  }
 
-    if (!isFF) {
-        css = [
-            "background-image: url('https://mutly.github.io/images/Leandro-R-Barbosa.jpg')",
-            "background-repeat: no-repeat",
-            "background-size: 205px 255px",
-            "margin: 0",
-            "padding: 0 205px 255px 0",
-            "font-size: 0"
-        ].join(';');
-        console.log("%c ", css);
-    }
+  document.title = str;
 
-    css = "font-size: 28px; color: #0055FF;";
-    console.log('%cLeandro R. Barbosa', css);
+  str = str.replace(/ \+/g, " </button> + <button>");
+  str = "<button> " + str + " </button>";
 
-    css = 'font-size: 12px;';
-    console.log('%cI\'m a front end developer.\nI focus on the finished product.\nI want things done fast, with quality.\nI like communication between teams.\nI like to do new things.', css);
+  document.querySelector(".command").innerHTML = str;
 
-    var c1 = 'color: #ff9900;',
-        c2 = 'color: #666';
+  // magic starts here
+  var weekday = function () {
+    var dayNames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+      now = new Date();
+    return dayNames[now.getDay()];
+  };
 
-    console.log(' ');
-    
-    console.log('%cWHat I do:', c1);
+  var css;
 
-    console.log('%c  ★ Front end developer;\n  ★ UX/UI;\n  ★ HTML5;\n  ★ JavaScript;\n  ★ CSS3.', c2);
+//   if (!isFF) {
+//     css = [
+//       "background-image: url('https://mutly.github.io/images/Leandro-R-Barbosa.jpg')",
+//       "background-repeat: no-repeat",
+//       "background-position: left top",
+//       "background-size: 205px 255px",
+//       "margin: 0",
+//       "padding: 0 205px 255px 0",
+//     ].join(";");
+//     console.log("%c ", css);
+//   }
 
-    console.log(' ');
+  css = "font-size: 28px; color: #0055FF;";
+  console.log("%cLeandro R. Barbosa", css);
 
-    console.log('You can reach me on...');
+  css = "font-size: 12px;";
+  console.log(
+    "%cI'm a front end developer.\nI focus on the finished product.\nI want things done fast, with quality.\nI like communication between teams.\nI like to do new things.",
+    css
+  );
 
-    css = 'color: #0000FF;';
-    console.log('%ctwitter.com/MuTLY', css);
-    console.log('%cfb.com/MuTLY', css);
-    console.log('%cleandro.barbosa@live.com', css);
+  var c1 = "color: #ff9900;",
+    c2 = "color: #666";
 
-    console.log(' ');
+  console.log(" ");
 
-    css = 'color: #FF0000';
-    console.log('Hope you\'re having a nice '+ weekday() +'.');
-    console.log('%c\u2764', css, 'Leandro');
+  console.log("%cWHat I do:", c1);
 
-    console.log(' ');
-}());
+  console.log(
+    "%c  ★ Front end developer;\n  ★ UX/UI;\n  ★ HTML5;\n  ★ JavaScript;\n  ★ CSS3.",
+    c2
+  );
+
+  console.log(" ");
+
+  console.log("You can reach me on...");
+
+  css = "color: #0000FF;";
+  console.log("%ctwitter.com/MuTLY", css);
+  console.log("%cfb.com/MuTLY", css);
+  console.log("%cleandro.barbosa@live.com", css);
+
+  console.log(" ");
+
+  css = "color: #FF0000";
+  console.log("Hope you're having a nice " + weekday() + ".");
+  console.log("%c\u2764", css, "Leandro");
+
+  console.log(" ");
+})();
